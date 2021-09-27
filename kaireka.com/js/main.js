007bbf41ee9cbd6dfb35d48448ad7546e29b073b
Zengord <dinar752@mail.ru>
2021-09-27 21:46:37 +0300

    Delete kaireka.com directory

--- kaireka.com/js/main.js
+++
@@ -1,22 +0,0 @@
-//@prepros-append jq-start.js
-//@prepros-append script.js
-//@prepros-append jq-end.js
-$(document).ready(function() {
-		var w=$(window).outerWidth();
-		var h=$(window).outerHeight();
-		var ua = window.navigator.userAgent;
-		var msie = ua.indexOf("MSIE ");
-		var isMobile = {Android: function() {return navigator.userAgent.match(/Android/i);},BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},Windows: function() {return navigator.userAgent.match(/IEMobile/i);},any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}};
-	function isIE() {
-		ua = navigator.userAgent;
-		var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
-		return is_ie; 
-	}
-	if(isIE()){
-		$('body').addClass('ie');
-	}
-	if(isMobile.any()){
-		$('body').addClass('touch');
-	}
-$('.wrapper').addClass('loaded');$('.icon-menu_gogo').click(function(event) {	$(this).toggleClass('active');	$('.menu_gogo__body').toggleClass('active');	$('body').toggleClass('lock');});function ibg(){	$.each($('.ibg'), function(index, val) {		if($(this).find('img').length>0){			$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');		}	});}ibg();
-});
