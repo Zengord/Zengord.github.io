1013a4be72e1f689bb6fd92d6061c3d476a957ac
Zengord <dinar752@mail.ru>
2021-10-02 20:28:17 +0300

    Add files via upload

---
+++ scuterelectric.com/js/main.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+	
+$('.wrapper').addClass('loaded');
+
+$('.icon-menu').click(function(event) {
+	$(this).toggleClass('active');
+	$('.menu__body').toggleClass('active');
+	$('body').toggleClass('lock');
+});
+
+
+});
