69fe1c39f59003aac7dee95ce67d8ed4173dd12c
Zengord <dinar752@mail.ru>
2021-10-03 16:09:23 +0300

    Add files via upload

---
+++ tuhingaomua.com/js/main.js
@@ -0,0 +1,1 @@
+!function(e){"use strict";e('[data-toggle="tooltip"]').tooltip({animated:"fade",placement:"top",container:"body"}),e.scrollUp({scrollText:'<i class="fa fa-angle-up"></i>',easingType:"linear",scrollSpeed:900,animation:"fade"}),jQuery("nav#dropdown").meanmenu()}(jQuery);
