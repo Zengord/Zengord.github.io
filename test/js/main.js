8d054b944cb03547f4611bab745e44d5cf5dd269
Zengord <dinar752@mail.ru>
2022-08-07 12:43:44 +0300

    Delete test directory

--- test/js/main.js
+++
@@ -1,9 +0,0 @@
-document.documentElement.className = '';
-
-function menuOpen() {
-  if (document.documentElement.className == 'menu-open') {
-    document.documentElement.className = '';
-  } else {
-    document.documentElement.className = 'menu-open';
-  }
-};
